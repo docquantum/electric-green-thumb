e0cc3c51957634f6493e39edc565149c063f69df
DocQuantum <shchurgood@gmail.com>
2020-01-18 18:57:44 -0600

    Implemented analog reads and DHT11 reading

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include <Arduino.h>
-// #include "DHT.h"
+#include "DHT.h"
+#include <Adafruit_Sensor.h>
 
 #define LIGHT_PIN   23  //D_out
 #define PUMP_PIN    22  //D_out
 #define DHT11_PIN   32  //D_in/out
 #define SOIL_PIN    39  //A_in
 #define WATER_PIN   34  //A_in
-#define PHOTO_PIN   35  //A_in
+#define PHOTO_PIN   36  //A_in
 
-// DHT dht(DHT11_PIN, DHT11);
+DHT dht(DHT11_PIN, DHT11);
 
 void setup() {
   pinMode(LIGHT_PIN, OUTPUT);
@@ -17,22 +18,24 @@ void setup() {
   pinMode(SOIL_PIN, INPUT);
   pinMode(WATER_PIN, INPUT);
   pinMode(PHOTO_PIN, INPUT);
+  dht.begin();
   Serial.begin(9600);
 }
 
 void loop() {
-  // Serial.print("PResistor Val= "); Serial.print(analogRead(PHOTO_PIN)); Serial.println("\n");
-  // Serial.print("Water level = "); Serial.print(analogRead(WATER_PIN)); Serial.println("\n");
-  // Serial.print(F("Temperature: "));
-  // Serial.print(dht.readTemperature(false,false));
-  // Serial.println(F("°C"));
+  Serial.print("PResistor Val= "); Serial.print(analogRead(PHOTO_PIN)); Serial.println("\n");
+  Serial.print("Soil Moisture = "); Serial.print(analogRead(SOIL_PIN)); Serial.println("\n");
+  Serial.print("Water level = "); Serial.print(analogRead(WATER_PIN)); Serial.println("\n");
+  Serial.print(F("Temperature: "));
+  Serial.print(dht.readTemperature(false,false));
+  Serial.println(F("°C"));
   
-  // // Get humidity event and print its value.
-  // Serial.print(F("Humidity: "));
-  // Serial.print(dht.readHumidity(false));
-  // Serial.println(F("%"));
+  // Get humidity event and print its value.
+  Serial.print(F("Humidity: "));
+  Serial.print(dht.readHumidity(false));
+  Serial.println(F("%\n"));
   
-  if(analogRead(PHOTO_PIN) < 150){
+  if(analogRead(PHOTO_PIN) < 800){
     digitalWrite(LIGHT_PIN, HIGH);
   } else{
     digitalWrite(LIGHT_PIN, LOW);
