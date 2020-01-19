fd2e949058db3947cdd47913d770cb64ec4cb557
DocQuantum <shchurgood@gmail.com>
2020-01-18 20:44:30 -0600

    Finalized sensor pins

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <Adafruit_Sensor.h>
 
 #define LIGHT_PIN   23  //D_out
-#define PUMP_PIN    22  //D_out
+#define PUMP_PIN    22  //D_out (inverted)
 #define DHT11_PIN   32  //D_in/out
 #define SOIL_PIN    39  //A_in
 #define WATER_PIN   34  //A_in
@@ -20,6 +20,19 @@ void setup() {
   pinMode(PHOTO_PIN, INPUT);
   dht.begin();
   Serial.begin(9600);
+  delay(1000);
+
+  Serial.println("Testing Light");
+  digitalWrite(LIGHT_PIN, HIGH);
+  delay(1000);
+  digitalWrite(LIGHT_PIN, LOW);
+  delay(1000);
+
+  Serial.println("Testing pump");
+  digitalWrite(PUMP_PIN, HIGH);
+  delay(1000);
+  digitalWrite(PUMP_PIN, LOW);
+  delay(1000);
 }
 
 void loop() {
@@ -35,7 +48,7 @@ void loop() {
   Serial.print(dht.readHumidity(false));
   Serial.println(F("%\n"));
   
-  if(analogRead(PHOTO_PIN) < 800){
+  if(analogRead(PHOTO_PIN) < 1200){
     digitalWrite(LIGHT_PIN, HIGH);
   } else{
     digitalWrite(LIGHT_PIN, LOW);
